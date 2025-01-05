92efb11f41ca38958fd7524d26c708534942bc83
V Daita <vdaita@gmail.com>
2025-01-04 20:48:04 -0600

    better visualiziations

---
+++ README.md
@@ -0,0 +1,1 @@
+Blog post at daita.com
--- visualize_results.py
+++ visualize_results.py
@@ -18,19 +18,33 @@ def main(filepath: str):
     table.add_column("Method", justify="center")
     table.add_column("Hits 0.95 at x", justify="center")
     table.add_column("Hits 0.9 at x", justify="center")
+    table.add_column("Relative to Average 0.95", justify="center")
+    table.add_column("Relative to Average 0.9", justify="center")
+
+
+    avg_idx_95 = np.where(np.array(results["averaged"]) >= 0.95)[0][0]
+    avg_idx_90 = np.where(np.array(results["averaged"]) >= 0.9)[0][0]
 
     for method in results:
         plt.plot(results[method], label=method)
         y_values = np.array(results[method])
         x_values = np.arange(1, len(y_values) + 1)
 
-        idx_95 = np.where(y_values >= 0.95)[0]
-        hit_95 = f"{(idx_95[0] + 1) / len(y_values) * 100:.2f}%" if len(idx_95) > 0 else "N/A"
+        idx_95 = np.where(y_values >= 0.95)[0][0]
+        pct_95 = (idx_95 + 1) / len(y_values) * 100
+        hit_95 = f"{pct_95:.2f}%" if len(y_values) > 0 else "N/A"
+        
 
-        idx_90 = np.where(y_values >= 0.9)[0]
-        hit_90 = f"{(idx_90[0] + 1) / len(y_values) * 100:.2f}%" if len(idx_90) > 0 else "N/A"
+        idx_90 = np.where(y_values >= 0.9)[0][0]
+        pct_90 = (idx_90 + 1) / len(y_values) * 100
+        hit_90 = f"{pct_90:.2f}%" if len(y_values) > 0 else "N/A"
 
-        table.add_row(method, hit_95, hit_90)
+        rel_avg_95 = (idx_95 - avg_idx_95) / avg_idx_95 * 100
+        rel_avg_90 = (idx_90 - avg_idx_90) / avg_idx_90 * 100
+        rel_avg_95 = f"[green]{rel_avg_95:.2f}%[green]" if rel_avg_95 <= 0 else f"[red]{rel_avg_95:.2f}%[red]"
+        rel_avg_90 = f"[green]{rel_avg_90:.2f}%[green]" if rel_avg_90 <= 0 else f"[red]{rel_avg_90:.2f}%[red]"
+
+        table.add_row(method, hit_95, hit_90, rel_avg_95, rel_avg_90)
 
     print(table)
 
